da0ea0a6c2b845e341960c4f0513a4b1b2f849ab
Mit Chin <mitchin10@gmail.com>
2024-03-26 21:10:37 -0400

    Update Script to generate prisma during deployment process

--- app/components/landingPage/AboutSection.tsx
+++ app/components/landingPage/AboutSection.tsx
@@ -11,12 +11,6 @@ const AboutSection = () => {
             <p>
                 Throughout my career, I&apos;ve had the opportunity to contributing to projects at Comcast, a leader in the telecommunications industry. This experience has allowed me to impact thousands of users with my work. I&apos;m passionate about building accessible web applications that enhance user experience and solve real-world problems.
             </p>
-            <p>
-                My expertise includes HTML, CSS, JavaScript,React, Node.js, and Next.js. As I continue to expand my portfolio, I&apos;m eager to apply these technologies in innovative ways to solve real-world problems. I&apos;m particularly excited about the potential of web applications to improve user experiences and contribute positively to our communities. My commitment to learning and professional growth drives me to explore new challenges and build meaningful projects in the web development space.
-            </p>
-            <p>
-                I believe in writing clean, maintainable code and in the power of teamwork to bring innovative ideas to life. I strive to bring creativity, efficiency, and reliability to every project I work on.
-            </p>
         </div>
     )
 }
