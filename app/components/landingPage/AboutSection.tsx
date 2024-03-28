cdd9a473f35dc25324c94057d3276aa3aa097a73
mitchin20 <163489879+mitchin20@users.noreply.github.com>
2024-03-28 14:31:02 -0400

    Add icons and SkillsSection (#4)
    Co-authored-by: Mit Chin <mitchin10@gmail.com>

--- app/components/landingPage/AboutSection.tsx
+++ app/components/landingPage/AboutSection.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const AboutSection = () => {
     return (
-        <div className='text-center'>
-            <h1
-                className='font-semibold my-8'
-            >
-                About Me
-            </h1>
-            <p>
+        <div className='text-lg text-center my-16'>
+            <div className='divider my-10'>
+                <h1
+                    className='font-semibold'
+                >
+                    About Me
+                </h1>
+            </div>
+            <p className='leading-8'>
                 Throughout my career, I&apos;ve had the opportunity to contributing to projects at Comcast, a leader in the telecommunications industry. This experience has allowed me to impact thousands of users with my work. I&apos;m passionate about building accessible web applications that enhance user experience and solve real-world problems.
             </p>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/landingPage/HeroSection";
 import AboutSection from "./components/landingPage/AboutSection";
+import SkillSection from "./components/landingPage/SkillSection";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center px-8 sm:px-24 md:px-56 lg:px-[300px] xl:px-[450px] 2xl:px-[550px]">
+        <div className="flex flex-col w-full items-center px-8 sm:px-24 md:px-56 lg:px-[300px] xl:px-[450px] 2xl:px-[550px]">
             <HeroSection />
-            <div className="bg-gray-300" style={{height: '80px', width: '2px'}}></div>
+            {/* <div className="bg-gray-300" style={{height: '80px', width: '2px'}}></div> */}
             <AboutSection />
+            <SkillSection />
         </div>
     )
 }
