c72c8429ab15371d0259db6fee8b62a9d10ef5c2
Mit Chin <mitchin10@gmail.com>
2024-03-30 00:02:42 -0400

    Update animation style

---
+++ app/components/landingPage/components/TechStackList.ts
@@ -0,0 +1,79 @@
+import { ReactIcon, NextIcon, HtmlIcon, CssIcon, JavaScriptIcon, NodeJsIcon, ApiIcon, DynamoDbIcon, MongoDbIcon, RestApiIcon, LambdaIcon, GitHubIcon, VercelIcon, DockerIcon, AgileIcon } from '../../svgs';
+
+export const TechStackList = [
+    {
+        name: "React",
+        icon: ReactIcon,
+        textColor: "text-sky-400"
+    },
+    {
+        name: "NextJS",
+        icon: NextIcon,
+        textColor: ""
+    },
+    {
+        name: "HTML",
+        icon: HtmlIcon,
+        textColor: "text-orange-600"
+    },
+    {
+        name: "CSS",
+        icon: CssIcon,
+        textColor: "text-[#1172B8]"
+    },
+    {
+        name: "JavaScript (ES6+)",
+        icon: JavaScriptIcon,
+        textColor: "text-[#F7DF1E]"
+    },
+    {
+        name: "NodeJS",
+        icon: NodeJsIcon,
+        textColor: "text-[#8CC84B]"
+    },
+    {
+        name: "Express",
+        icon: ApiIcon,
+        textColor: ""
+    },
+    {
+        name: "DynamoDB",
+        icon: DynamoDbIcon,
+        textColor: "text-[#2D72B8]"
+    },
+    {
+        name: "MongoDB",
+        icon: MongoDbIcon,
+        textColor: "text-[#58AA50]"
+    },
+    {
+        name: "RESTful API Design",
+        icon: RestApiIcon,
+        textColor: ""
+    },
+    {
+        name: "AWS Lambda",
+        icon: LambdaIcon,
+        textColor: "text-[#FA7E14]"
+    },
+    {
+        name: "Git & GitHub",
+        icon: GitHubIcon,
+        textColor: ""
+    },
+    {
+        name: "Vercel",
+        icon: VercelIcon,
+        textColor: ""
+    },
+    {
+        name: "Docker",
+        icon: DockerIcon,
+        textColor: "text-[#1794D4]"
+    },
+    {
+        name: "Agile Methodologies",
+        icon: AgileIcon,
+        textColor: ""
+    },
+]
