d0a27eebc84414f2884f02f67be71992b08039a6
Mit Chin <mitchin10@gmail.com>
2024-04-01 11:42:34 -0400

    Add animation to tech stack layout

--- app/components/landingPage/components/LargeScreen.tsx
+++ app/components/landingPage/components/LargeScreen.tsx
@@ -4,6 +4,18 @@ import { TechStackList } from './TechStackList';
 import { useAppSelector } from '@/app/redux/hooks';
 import { RootState } from '../../../redux/store';
 import useDetectScreenSize from './useDetectScreenSize';
+import { motion } from 'framer-motion';
+
+const upVariants = {
+    hidden: {
+        y: 100,
+        opacity: 0
+    },
+    visible: {
+        y: 0,
+        opacity: 1
+    }
+}
 
 type TechStackList = {
     name: string;
@@ -17,22 +29,37 @@ const LargeScreen = () => {
     useDetectScreenSize(screenSize);
     const isScreenSmall = useAppSelector((state: RootState) => state.isScreenSmall.value);
 
-    let chunkSize = isScreenSmall ? 2 : 3;
+    let groupSize = isScreenSmall ? 2 : 3;
 
-    const chunkArr = (arr: Array<TechStackList>) => {
+    // Based on the screen size
+    // Different number of element in a group will be displayed in a row
+    const groupArr = (arr: Array<TechStackList>) => {
         const result = [];
-        for (let i = 0; i < arr.length; i += chunkSize) {
-            const chunk = arr.slice(i, i + chunkSize);
-            result.push(chunk);
+        for (let i = 0; i < arr.length; i += groupSize) {
+            const group = arr.slice(i, i + groupSize);
+            result.push(group);
         }
         return result;
     }
 
-    const chunkedArr = chunkArr(TechStackList);
+    const chunkedArr = groupArr(TechStackList);
     return (
         <div>
             {chunkedArr.map((row, rowIndex) => (
-                <div key={rowIndex} className="stats shadow flex content-center items-center mb-5">
+                <motion.div 
+                    key={rowIndex} 
+                    initial="hidden"
+                    whileInView="visible"
+                    variants={upVariants}
+                    transition={{
+                        duration: 0.5
+                    }}
+                    viewport={{
+                        once: false,
+                        amount: 0.5
+                    }}
+                    className="stats shadow flex content-center items-center mb-5 "
+                >
                     {row.map(({name, icon: Icon, textStyle}, index) => (
                         <div key={index} className='stat'>
                             <div className="stat-figure">
@@ -43,7 +70,7 @@ const LargeScreen = () => {
                             </div>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             ))}
         </div>
     )
