8f36ab16ec2a55d6c7bc2999514ab9cb70d6a538
Mit Chin <mitchin10@gmail.com>
2024-04-22 18:36:18 -0400

    Conditional render Navbar

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import NavItemsCenter from './NavItemsCenter';
 
 const NavBar = () => {
+    const [currentPath, setCurrentPath] = useState<string>('');
+
+    useEffect(() => {
+        setCurrentPath(window.location.pathname);
+
+        const handleChangePath = () => {
+            setCurrentPath(window.location.pathname);
+        }
+
+        window.addEventListener('popstate', handleChangePath);
+        // Listening to hashchange if you expect to work with hash-based routing
+        window.addEventListener('hashchange', handleChangePath);
+
+        return () => {
+            window.removeEventListener('popstate', handleChangePath);
+            window.removeEventListener('hashchange', handleChangePath);
+        };
+    }, [])
+
     return (
-        <div className="navbar sticky bg-transparent top-0 z-30">
-            <div className='navbar-center rounded-full border mx-auto border-slate-200 text-gray-500 bg-base-100'>
-                <NavItemsCenter />
-            </div>
-        </div>
+        <>
+            {currentPath !== "/signin" ? (
+                <div className="navbar sticky bg-transparent top-0 z-30">
+                    <div className='navbar-center rounded-full border mx-auto border-slate-200 text-gray-500 bg-base-100'>
+                        <NavItemsCenter />
+                    </div>
+                </div>
+            ) : null}
+        </>
     )
 }
 
