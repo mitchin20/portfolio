5d3c33d0927a5129ca872eadae3ca2a1311c4005
Mit Chin <mitchin10@gmail.com>
2024-04-22 22:33:08 -0400

    Remove console log

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -13,8 +13,6 @@ export async function POST(req: Request) {
         where: { email: email }
     })
 
-    console.log("Existing User", existingUser);
-
     if (existingUser) {
         return Response.json({
             error: "Email already existed."
