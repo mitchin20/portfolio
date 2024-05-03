f65436e1d0167f2b10023f33611e5dfa65f304fd
Mit Chin <mitchin10@gmail.com>
2024-05-03 15:40:20 -0400

    Update Navbar component

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,38 +1,13 @@
-"use client";
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NavItemsCenter from './NavItemsCenter';
 
 const NavBar = () => {
-    const [currentPath, setCurrentPath] = useState<string>('');
-
-    useEffect(() => {
-        setCurrentPath(window.location.pathname);
-
-        const handleChangePath = () => {
-            setCurrentPath(window.location.pathname);
-        }
-
-        window.addEventListener('popstate', handleChangePath);
-        // Listening to hashchange if you expect to work with hash-based routing
-        window.addEventListener('hashchange', handleChangePath);
-
-        return () => {
-            window.removeEventListener('popstate', handleChangePath);
-            window.removeEventListener('hashchange', handleChangePath);
-        };
-    }, [])
-
     return (
-        <>
-            {currentPath !== "/signin" ? (
-                <div className="navbar sticky bg-transparent top-0 z-30">
-                    <div className='navbar-center rounded-full border mx-auto border-slate-200 text-gray-500 bg-base-100'>
-                        <NavItemsCenter />
-                    </div>
-                </div>
-            ) : null}
-        </>
+        <div className="navbar sticky bg-transparent top-0 z-30">
+            <div className='navbar-center rounded-full border mx-auto border-slate-200 text-gray-500 bg-base-100'>
+                <NavItemsCenter />
+            </div>
+        </div>
     )
 }
 
