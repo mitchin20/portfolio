2980200818b15c899be44af8265479cc8c1995a1
gndesktop <mitchin20@gmail.com>
2024-05-28 09:14:05 -0400

    Fix overflow-x issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import LandingPage from "./components/landingPage/LandingPage";
 
 export default function Home() {
     return (
-        <div className="w-full overflow-hidden">
-            <LandingPage />
-        </div>
+        <LandingPage />
     )
 }
