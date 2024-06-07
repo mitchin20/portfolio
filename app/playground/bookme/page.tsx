3752f9018568e3d2d100e7af694fae23abc1f1cf
gndesktop <mitchin20@gmail.com>
2024-06-06 20:45:19 -0400

    Setup layout for the playground page

---
+++ app/playground/bookme/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Bookme = () => {
+    return (
+        <div>
+            Book Me
+        </div>
+    )
+}
+
+export default Bookme;
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
 
-const Playground = () => {
-  return (
-    <div>Playground</div>
-  )
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import { Alert } from '@mui/material';
+
+export default function Playground() {
+
+    return (
+        <div>
+            <div
+                className='sm:text-sm'
+            >
+                <Alert severity='info'>
+                    <Typography paragraph>
+                        Welcome to the Playground!
+                    </Typography>
+                    <Typography paragraph>
+                        Here, you can explore and test different projects I&apos;ve built.
+                    </Typography>
+                    <Typography paragraph>
+                        Dive in and have fun!
+                    </Typography>
+                </Alert>
+            </div>
+        </div>
+    );
 }
-
-export default Playground
